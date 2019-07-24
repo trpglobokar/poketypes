14cd0a1dc3ecfd0b1cc460791b348c95814e14bc
Rocky PG <trp.globokar@gmail.com>
2019-07-24 16:43:17 -0700

    feat(ChordChart): made base chart

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./App.css"
+import ChordChart from "./chord-chart.js"
+import pokejson from "./pokemon.json"
+import poketypejson from "./pokemon-types.json"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let pokematrix = []
+let pokematrix2 = []
+for (const i in poketypejson) {
+  pokematrix[i] = []
+  pokematrix2[i] = []
+  for (const j in poketypejson) {
+    pokematrix[i][j] = 0
+    pokematrix2[i][j] = []
+  }
 }
 
-export default App;
+console.log("pokematrix", pokematrix)
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    // create input data: a square matrix that provides flow between entities
+    //for(const poke of pokejson) {
+    for (let i = 0; i < 800; i++) {
+      let poke = pokejson[i]
+      if (poke.type.length === 1) {
+        const index0 = poketypejson.findIndex(to => to.name === poke.type[0])
+        pokematrix[index0][index0] = pokematrix[index0][index0] + 1
+        pokematrix2[index0][index0].push(poke)
+      } else {
+        const index1 = poketypejson.findIndex(to => to.name === poke.type[0])
+        const index2 = poketypejson.findIndex(to => to.name === poke.type[1])
+        pokematrix[index1][index2] = pokematrix[index1][index2] + 1
+        pokematrix[index2][index1] = pokematrix[index2][index1] + 1
+        pokematrix2[index1][index2].push(poke)
+        pokematrix2[index2][index1].push(poke)
+      }
+    }
+    this.state = {
+      pokematrix,
+      pokematrix2,
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <ChordChart
+          pokematrix={this.state.pokematrix}
+          pokematrix2={this.state.pokematrix2}
+        />
+        <div id="type_container">Bob</div>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/chord-chart.js
@@ -0,0 +1,206 @@
+import React from "react"
+import * as d3 from "d3"
+import poketypejson from "./pokemon-types.json"
+import "./App.css"
+
+class ChordChart extends React.Component {
+  componentDidMount() {
+    this.drawChart()
+  }
+
+  addLabel(
+    group,
+    className,
+    tickInterval,
+    endMarker,
+    fontSize,
+    outerPosition,
+    getContent,
+    appendContent
+  ) {
+    const gMan = group
+      .selectAll(className)
+      .data(d => getContent(d, tickInterval, endMarker))
+      .enter()
+      .append("g")
+      .attr("transform", function(d) {
+        return (
+          "rotate(" +
+          ((d.angle * 180) / Math.PI - 90) +
+          ") translate(" +
+          outerPosition +
+          ",0)"
+        )
+      })
+    appendContent(gMan, fontSize)
+  }
+
+  appendLine(gMan) {
+    gMan
+      .append("line") // By default, x1 = y1 = y2 = 0, so no need to specify them
+      .attr("x2", 6)
+      .attr("stroke", "black")
+  }
+
+  appendText(gMan, fontSize) {
+    gMan
+      .append("text")
+      .attr("x", 8)
+      .attr("dy", ".35em")
+      .attr("transform", d =>
+        d.angle > Math.PI ? "rotate(180) translate(-16)" : null
+      )
+      .style("text-anchor", d => (d.angle > Math.PI ? "end" : null))
+      .text(d => d.value)
+      .style("font-size", fontSize)
+  }
+
+  // Returns array of tick angles & values for a given group and step
+  getTicks(d, step, endMarker) {
+    const k = (d.endAngle - d.startAngle) / 100
+    return d3.range(0, endMarker, step).map(function(value) {
+      return { value: `${value}%`, angle: value * k + d.startAngle }
+    })
+  }
+
+  // Return single array of label angle & value per group
+  getTypeLabels(d) {
+    const k = (d.endAngle - d.startAngle) / 2
+    return [{ value: poketypejson[d.index].name, angle: k + d.startAngle }]
+  }
+
+  drawChart() {
+    const { pokematrix, pokematrix2 } = this.props
+
+    // Creates the svg area
+    const svg = d3
+      .select("#chord-chart")
+      .append("svg")
+      .attr("width", 640)
+      .attr("height", 640)
+      .append("g")
+      .attr("transform", "translate(320,320)")
+
+    // Adds a blank tooltip div
+    const tooltip = d3
+      .select("#chord-chart")
+      .append("div")
+      .attr("class", "tooltip")
+      .style("background-color", "white")
+      .style("border", "solid")
+      .style("border-width", "1px")
+      .style("border-radius", "5px")
+      .style("padding", "10px")
+
+    const showTooltip = function(d) {
+      const pokemon = pokematrix2[d.source.index][d.target.index]
+      const pokenames = pokemon.map(poke => {
+        return `<img src="https://img.pokemondb.net/sprites/sun-moon/icon/${poke.name.english.toLowerCase()}.png" /> ${
+          poke.name.english
+        }`
+      })
+      tooltip
+        .style("opacity", 1)
+        .html(
+          "Type 1: " +
+            poketypejson[d.source.index].name +
+            "<br>Type 2: " +
+            poketypejson[d.target.index].name +
+            "<br><br>Num of Pokes:" +
+            (d.source.value + d.target.value) / 2 +
+            "<br><br>" +
+            pokenames.join("<br /> ")
+        )
+        .style("left", d3.event.pageX + 15 + "px")
+        .style("top", d3.event.pageY - 28 + "px")
+    }
+
+    // Passes matrix to d3.chord(); calculates info needed to draw arc and ribbon
+    const res = d3
+      .chord()
+      .padAngle(0.05) // padding between entities (black arc)
+      .sortSubgroups(d3.descending)(pokematrix)
+
+    // Add the links between groups
+    svg
+      .datum(res)
+      .append("g")
+      .selectAll("path")
+      .data(d => d)
+      .enter()
+      .append("path")
+      .attr("d", d3.ribbon().radius(200))
+      .style("fill", d => `${poketypejson[d.source.index].color}55`)
+      .on("mouseover", showTooltip)
+    //.on("mouseleave", hideTooltip )
+
+    // Declare group object that represents each type group
+    const group = svg
+      .datum(res)
+      .append("g")
+      .selectAll("g")
+      .data(d => d.groups)
+      .enter()
+
+    // Add type group arcs on circle rim
+    group
+      .append("g")
+      .append("path")
+      .style("fill", (_d, i) => poketypejson[i].color)
+      .attr(
+        "d",
+        d3
+          .arc()
+          .innerRadius(180)
+          .outerRadius(200)
+      )
+
+    // Add the ticks
+    this.addLabel(
+      group,
+      ".group-tick",
+      12.5,
+      101,
+      9,
+      200,
+      this.getTicks,
+      this.appendLine
+    )
+
+    // Add a couple tick labels:
+    this.addLabel(
+      group,
+      ".group-tick-label",
+      25,
+      100,
+      9,
+      200,
+      this.getTicks,
+      this.appendText
+    )
+    // Adds type labels to groups
+    this.addLabel(
+      group,
+      ".group-type-label",
+      1,
+      2,
+      12,
+      230,
+      this.getTypeLabels,
+      this.appendText
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        <div
+          id="chord-chart"
+          style={{ display: "flex", flexDirection: "row" }}
+        />
+      </div>
+    )
+  }
+}
+
+export default ChordChart
