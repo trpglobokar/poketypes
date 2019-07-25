91a00e5d1886a194cfa9dae75d7265dfa8771619
Rocky PG <trp.globokar@gmail.com>
2019-07-24 23:10:02 -0700

    Updates

---
+++ precache-manifest.c6422bd7d413711d8fce81c2ddce4601.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db7dbc57c9a88ddf7e8c6c035e72d2f6",
+    "url": "/poketypes/index.html"
+  },
+  {
+    "revision": "f24869f7384636d2de6d",
+    "url": "/poketypes/static/css/main.94ad8322.chunk.css"
+  },
+  {
+    "revision": "33ec1a2cfd172001d5d4",
+    "url": "/poketypes/static/js/2.5216ddc2.chunk.js"
+  },
+  {
+    "revision": "f24869f7384636d2de6d",
+    "url": "/poketypes/static/js/main.05e6d8dd.chunk.js"
+  },
+  {
+    "revision": "f4ccf180986d8f7a9ee6",
+    "url": "/poketypes/static/js/runtime~main.7855d92a.js"
+  }
+]);
